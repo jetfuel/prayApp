d41459a801cc8c64df24af824bbae5083e47e2c7
Jeff Wang <jeffwang@yp.com>
2013-08-13 17:21:41 -0700

    Added AFNetworking

---
+++ Service/PANetWorking.h
@@ -0,0 +1,13 @@
+//
+//  PANetWorking.h
+//  prayApp
+//
+//  Created by Jeff Wang on 8/13/13.
+//  Copyright (c) 2013 Jeff Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PANetWorking : NSObject
+
+@end
---
+++ Service/PANetWorking.m
@@ -0,0 +1,13 @@
+//
+//  PANetWorking.m
+//  prayApp
+//
+//  Created by Jeff Wang on 8/13/13.
+//  Copyright (c) 2013 Jeff Wang. All rights reserved.
+//
+
+#import "PANetWorking.h"
+
+@implementation PANetWorking
+
+@end
